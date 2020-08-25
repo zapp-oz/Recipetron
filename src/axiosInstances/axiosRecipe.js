249b1f38a5ff05f25f278d9adc01292cbd7861eb
shresth dewan <shresthdewan@gmail.com>
2020-08-25 22:29:25 +0530

    Add recipe api fetch

---
+++ src/axiosInstances/axiosRecipe.js
@@ -0,0 +1,27 @@
+const axios = require("axios")
+
+const axiosRecipe = axios.create({
+    baseURL: "https://api.spoonacular.com/recipes/complexSearch",
+    timeout: 30000,
+    headers: {
+        'Content-Type': 'application/json',
+      }
+})
+
+// const reqInterceptor = axiosRecipe.interceptors.request.use(req => {
+//     console.log("req", req)
+//     return Promise.resolve(req)
+// }, err => {
+//     console.log("err", err)
+//     return Promise.reject(err)
+// })
+
+// const resInterceptor = axiosRecipe.interceptors.response.use(res => {
+//     // console.log("res", res.data.results)
+//     return Promise.resolve(res)
+// }, err => {
+//     console.log("err", err)
+//     return Promise.reject(err)
+// })
+
+module.exports = axiosRecipe
